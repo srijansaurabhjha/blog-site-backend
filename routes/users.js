7fdb4e654676953733d7f29eaea00cdf7d1ba4e0
srijansaurabhjha <srijanjha5@gmail.com>
2023-04-29 09:58:06 +0530

    added bcryptjs

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const router=require("express").Router();
 const User=require("../models/User.js");
 const Post=require("../models/Post.js");
-const bcrypt=require('bcrypt');
+const bcrypt=require('bcryptjs');
 
 //UPDATE
 router.put("/:id",async(req,res)=>{
